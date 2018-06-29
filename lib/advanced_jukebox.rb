9da941610296212c4a4f9dee5d6482e58e52de2e
Jesse McReady <jessemcready@gmail.com>
2018-06-29 10:40:55 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -26,6 +26,9 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
+  my_songs.each do |song, path|
+    puts song
+  end 
 end
 
 
