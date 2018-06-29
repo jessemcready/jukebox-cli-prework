8183db534b46bbcad2ff21f3f28ffa87a3a83a21
Jesse McReady <jessemcready@gmail.com>
2018-06-29 10:15:30 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -19,7 +19,7 @@ def help
 end 
 
 def play(songs)
-  puts "Please enter a song name or number:"
+  #puts "Please enter a song name or number:"
   input = gets.chomp
   if input.is_a?(Integer)
     if input > songs.length || input <= 0
